f9392ed19af6cabe83e5abc18528aaa1a70fafe1
AnsonLoveLina <326236882@qq.com>
2016-05-09 16:04:25 +0800

    Initial commit

---
+++ src/main/java/cn/sinobest/Solution.java
@@ -0,0 +1,60 @@
+package cn.sinobest;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by zhouyi1 on 2016/5/9 0009.
+ */
+public class Solution {
+
+    /**
+     * 347. Top K Frequent Elements
+     * Given a non-empty array of integers, return the k most frequent elements.
+     *
+     * For example,
+     * Given [1,1,1,2,2,3] and k = 2, return [1,2].
+     *
+     * Note:
+     * You may assume k is always valid, 1 ≤ k ≤ number of unique elements.
+     * Your algorithm's time complexity must be better than O(n log n), where n is the array's size.
+     * @param nums
+     * @param k
+     * @return
+     */
+    public List<Integer> topKFrequent(int[] nums, int k) {
+        List<Integer>[] fNums = new List[nums.length+1];
+        
+
+        return null;
+    }
+
+    /**
+     * 343. Integer Break
+     * @param n
+     * @return
+     */
+    public int integerBreak(int n) {
+
+        return 0;
+    }
+
+    /**
+     * 30. Substring with Concatenation of All Words
+     * You are given a string, s, and a list of words, words, that are all of the same length. Find all starting indices of substring(s) in s that is a concatenation of each word in words exactly once and without any intervening characters.
+     *
+     * For example, given:
+     * s: "barfoothefoobarman"
+     * words: ["foo", "bar"]
+     *
+     * You should return the indices: [0,9].
+     * @param s
+     * @param words
+     * @return
+     */
+    public List<Integer> findSubstring(String s, String[] words) {
+        return null;
+    }
+}
