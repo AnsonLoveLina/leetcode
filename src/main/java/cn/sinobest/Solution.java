1627bd20f6951278aa1b50c46d691d1fa9223aa8
AnsonLoveLina <326236882@qq.com>
2016-06-23 17:43:18 +0800

    maxEnvelopes提交

--- src/main/java/cn/sinobest/Solution.java
+++ src/main/java/cn/sinobest/Solution.java
@@ -16,6 +16,57 @@ import java.util.Map;
 public class Solution {
 
     /**
+     * You have a number of envelopes with widths and heights given as a pair of integers (w, h). One envelope can fit into another if and only if both the width and height of one envelope is greater than the width and height of the other envelope.
+
+     What is the maximum number of envelopes can you Russian doll? (put one inside other)
+
+     Example:
+     Given envelopes = [[5,4],[6,4],[6,7],[2,3]], the maximum number of envelopes you can Russian doll is 3 ([2,3] => [5,4] => [6,7]).
+
+     Subscribe to see which companies asked this question
+     * @param envelopes
+     * @return
+     */
+    public int maxEnvelopes(int[][] envelopes) {
+        List<Envelopes> envelopesList = new ArrayList<Envelopes>();
+        int level = 0;
+        for (int i = 0; i < envelopes.length; i++) {
+            if (envelopes[i].length!=2){
+                return 0;
+            }
+            int width = envelopes[i][0];
+            int length = envelopes[i][1];
+            Envelopes envelopes1 = new Envelopes(new int[][]{envelopes[i]},width+length);
+//            if ()
+        }
+        return level;
+    }
+
+    class Envelopes{
+        int [][] widthAndLength;
+        int sum;
+
+        public Envelopes(int[][] widthAndLength, int sum) {
+            this.widthAndLength = widthAndLength;
+            this.sum = sum;
+        }
+
+        public void addWidthAndLength(int[][] widthAndLength){
+
+        }
+
+        @Override
+        public int hashCode() {
+            return sum;
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            return sum == ((Envelopes)obj).sum;
+        }
+    }
+
+    /**
      * 347. Top K Frequent Elements
      * Given a non-empty array of integers, return the k most frequent elements.
      *
